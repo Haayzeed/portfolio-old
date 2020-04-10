73d04d3d26656bd6a19e653a9caabd38d9fcb732
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-04-10 04:05:58 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My Portfolio - Home Page</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Lora|Montserrat:900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/aos.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 wrapper banner">
+				<div class="text-wrapper" >
+					<h1 class="">Afeez Dosunmu </h1>
+					<p class="">Front-End developer</p>
+				</div>
+			</div>
+			<div class="col-md-12 wrapper">
+				<div class="text-wrapper" style="width: 80vw">
+					<h1 class="" data-aos="fade-down" data-aos-duration="1000">Who am I</h1>
+					<p class="" data-aos="fade-down" data-aos-duration="1000">I am a web developer, I design and code User Interfaces for web and mobile apps, and I love what I do.</p>
+				</div>
+			</div>
+			<div class="col-md-12 wrapper">
+				<div class="text-wrapper" style="width: 80vw">
+					<h1 class="" data-aos="fade-down" data-aos-duration="1000">Qualification</h1>
+					<p class="" data-aos="fade-down" data-aos-duration="1000">Computer Science</p>
+					<p class="" data-aos="fade-down" data-aos-duration="1000">Federal Polytechnic, Ilaro</p>
+				</div>
+			</div>
+			<div class="col-md-12 wrapper">
+				<div class="text-wrapper">
+					<h1 class="text-center" data-aos="fade-down" data-aos-duration="1000">Services</h1>
+					<p class="text-center" data-aos="fade-down" data-aos-duration="1000">Web Development</p>
+					<p class="text-center" data-aos="fade-down" data-aos-duration="1000">Android UI Developement</p>
+					<p class="text-center" data-aos="fade-down" data-aos-duration="1000">Wordpress Development</p>
+					<p class="text-center" data-aos="fade-down" data-aos-duration="1000">Plugin Implementation</p>
+					<p class="text-center" data-aos="fade-down" data-aos-duration="1000">Search Engine Optimization</p>
+				</div>
+			</div>
+			<div class="col-md-12 wrapper">
+				<div class="text-wrapper">
+					<h1 class="text-center" data-aos="fade-down" data-aos-duration="1000">Contact</h1>
+					<div class="col-md-12" data-aos="fade-down" data-aos-duration="1000">
+						<form>
+							<div class="form-group">
+								<input type="text" name="" class="form-control rounded-0" placeholder="Email">
+							</div>
+							<div class="form-group">
+								<input type="text" name="" class="form-control rounded-0" placeholder="Message">
+							</div>
+							<div class="form-group">
+								<button type="submit" class="btn btn-primary rounded-0 btn-block font-weight-bold" style="background-color: #20BF55; border: none">Send Message</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="particles-js"></div>
+
+</body>
+<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/aos.js"></script>
+<script type="text/javascript">
+	$(function(){
+	AOS.init();
+	});
+</script>
+<script type="text/javascript" src="js/particles.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+</html>
+<style type="text/css">
+	#particles-js{
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		/*z-index: -1;*/
+		background-position:cover;
+		/*background-color: green;*/
+	}
+</style>
